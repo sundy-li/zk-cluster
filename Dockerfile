cceadb0a05f7dcfc3a3eba2f5ab9699fa4e36eed
sundy <sundyli@truxing.com>
2017-05-19 17:38:11 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM openjdk:8-jre-alpine
+MAINTAINER sundyli <543950155@qq.com>
+
+ARG VERSION=3.4.10
+WORKDIR /opt/zookeeper
+
+EXPOSE 2181 2888 3888
+
+##download install
+RUN apt-get install -y wget tar 
+	&& wget https://mirrors.tuna.tsinghua.edu.cn/apache/zookeeper/zookeeper-${version}/zookeeper-${version}.tar.gz \
+	&& tar  -C /opt/zookeeper --strip-components=1 -xzvf zookeeper-${version}.tar.gz \
+	&& rm zookeeper-${version}.tar.gz \
+	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+
+##default config
+ENV tickTime=2000
+ENV dataDir=/data/zookeeper/
+ENV dataLogDir=/var/log/zookeeper/
+ENV clientPort=2181
+ENV initLimit=5
+ENV syncLimit=2
+
+# add startup script
+ADD entrypoint.sh entrypoint.sh
+RUN chmod +x entrypoint.sh
+
+ENTRYPOINT ["/usr/share/zookeeper/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## ZK_ID is the id in myid file, ZK_SERVERS is the the zk cluster servers in zoo.cfg
+export ZK_ID=$1
+export ZK_SERVERS=$2
+
+mkdir -p $dataDir
+mkdir -p $dataLogDir
+
+echo "${ZK_ID}" > $dataDir/myid
+
+#build zookeeper config file
+ZOOKEEPER_CONFIG=
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"tickTime=$tickTime"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"dataDir=$dataDir"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"dataLogDir=$dataLogDir"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"clientPort=$clientPort"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"initLimit=$initLimit"
+ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"syncLimit=$syncLimit"
+
+zkHosts=`echo $ZK_SERVERS  | awk -F, 'OFS=" "{$1=$1;print $0}'`
+for index in "${!zkHosts[@]}"
+do
+    ZKID=$(($index+1))
+    ZKIP=${ZOOKEEPER_SERVERS_ARRAY[index]}
+    ZOOKEEPER_CONFIG="$ZOOKEEPER_CONFIG"$'\n'"server.$ZKID=$ZKIP:2888:3888"
+done
